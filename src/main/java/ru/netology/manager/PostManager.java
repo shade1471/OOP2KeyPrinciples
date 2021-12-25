9f03115283b86e0733d9a3773648eb5f57a7c84e
Andrey Gribanov <shade1471@gmail.com>
2021-12-26 00:30:16 +0700

    Add PostManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,16 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] searchPosts(int ownerId, String domain, String query, int ownersOnly, int count, int offset) {
+        // logic
+        return null;
+    }
+
+    public void deletePost(int ownerId, int postId) {
+        // logic
+    }
+}
